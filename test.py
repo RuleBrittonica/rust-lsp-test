5eace4845989d9f25dc879441cc2b6ce3f2c3804
Matthew Britton <mbritton460@gmail.com>
2024-09-28 01:32:46 +1000

    Updated venv

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-import pexpect
+import wexpect
 
-child = pexpect.spawn('echo Hello, World!')
-child.expect(pexpect.EOF)
+# Start a child process running the command `echo Hello, World!`
+child = wexpect.spawn('echo Hello, World!')
+
+# Wait for the process to finish and reach the end of output (EOF)
+child.expect(wexpect.EOF)
+
+# Output up to the EOF is captured in child.before, which is then decoded from bytes to a string
 print(child.before.decode())
